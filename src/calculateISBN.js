efa98facb7c21799b0940b30b05932cab37cbffd
Leonie Hein <hein.leonie@gmx.net>
2022-07-14 12:57:02 +0200

    Tests für Länge der ISBN

--- src/calculateISBN.js
+++ src/calculateISBN.js
@@ -1,18 +1,22 @@
-function calculateISBNChecksum(num){
-    numArray = num.split('')
-    var output = []
-    var calc = 0
-    var checkSum = ""
+function calculateISBNChecksum(num) {
+  numArray = num.split("");
+  var output = [];
+  var calc = 0;
+  var checkSum = "";
 
-    for(let i = 0; i < 9; i++){
-        output[i] = parseInt(numArray[i]) * (i + 1)
-        calc += output[i]
-    }
+  if (num.length != 10) {
+    return "Die ISBN darf nur aus 10 Ziffern bestehen";
+  } else {
+        for (let i = 0; i < 9; i++) {
+        output[i] = parseInt(numArray[i]) * (i + 1);
+        calc += output[i];
+        }
 
-    checkSum = calc % 11
-    return checkSum.toString()
+    checkSum = calc % 11;
+    return checkSum.toString();
+  }
 }
 
 module.exports = {
-    calculateISBNChecksum
-}
+  calculateISBNChecksum,
+};
--- src/calculateISBN.test.js
+++ src/calculateISBN.test.js
@@ -33,3 +33,18 @@ describe("calculateISBNCheckSum", () => {
     });
   });
 });
+
+//Tests für die Länge der eingegebenen ISBN
+describe("calculateISBNChecksum", () => {
+  [
+    { input: "3423348962348"},
+    { input: "34233496246"},
+    { input: "342334861"},
+    { input: "34232090"}
+  ]
+    .forEach(({ input }) => {
+      it("should return Error", () => {
+        expect(calculateISBNChecksum(input)).toBe("Die ISBN darf nur aus 10 Ziffern bestehen"); // nimmt input entgegen und prüft, dass output herauskommt
+      });
+    });
+});
