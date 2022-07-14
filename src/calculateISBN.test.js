654da11455b1a20267c2c1ef9d84d168a0a6902a
Leonie Hein <hein.leonie@gmx.net>
2022-07-14 12:52:18 +0200

    Tests für ISBN mit X am Ende

--- src/calculateISBN.test.js
+++ src/calculateISBN.test.js
@@ -1,6 +1,5 @@
 const { calculateISBNChecksum } = require("./calculateISBN");
 describe("calculateISBN", () => {
-    
   it("should work", () => {
     console.log("Hallo Ansbach");
   });
@@ -8,15 +7,29 @@ describe("calculateISBN", () => {
 
 describe("calculateISBNChecksum", () => {
   [
-    {input: '3423348968', output: '8'},
-    {input: '3423349646', output: '6'},
-    {input: '3423345861', output: '1'},
-    {input: '3423209690', output: '0'}
+    { input: "3423348968", output: "8" },
+    { input: "3423349646", output: "6" },
+    { input: "3423345861", output: "1" },
+    { input: "3423209690", output: "0" },
   ]
-  //Test wird für jede Zeile oben angewandt
-  .forEach(({input, output}) => {
-    it("should return output", () => {
-      expect(calculateISBNChecksum(input)).toBe(output); // nimmt input entgegen und prüft, dass output herauskommt
-    })
-  })
+    //Test wird für jede Zeile oben angewandt
+    .forEach(({ input, output }) => {
+      it("should return output", () => {
+        expect(calculateISBNChecksum(input)).toBe(output); // nimmt input entgegen und prüft, dass output herauskommt
+      });
+    });
+});
+
+//Tests für ISBN-Nummern, die ein X am Ende enthalten
+describe("calculateISBNCheckSum", () => {
+  [
+    { input: "342334976X", output: "10" },
+    { input: "164780342X", output: "10" },
+    { input: "395051032X", output: "10" },
+  ]
+    .forEach(({ input, output }) => {
+      it("should return 10 for output", () => {
+        expect(calculateISBNChecksum(input)).toBe(output);
+    });
+  });
 });
